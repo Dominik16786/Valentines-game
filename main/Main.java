9bdf7f5308d0fcaf8969ae87585e7f1d3720e377
Dominik Papaj <dominikpapaj@dominiks-laptop.home>
2025-08-20 23:31:41 +0200

    commit

---
+++ main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main{
+    public static void main(String[] args){
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false); //to resize the window will see later 
+        window.setTitle("Szczęśliwych walentynek");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.startGameThread();
+    }
+}
